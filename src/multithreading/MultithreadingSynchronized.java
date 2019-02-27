02c2363cf57058f18a1e07a13d43ebddd0af7752
marv <marvelmd@gmail.com>
2019-02-27 19:30:12 +0300

    MultithreadingSynchronized firtt part where we sycing on .htis object

---
+++ src/multithreading/MultithreadingSynchronized.java
@@ -0,0 +1,41 @@
+package multithreading;
+
+public class MultithreadingSynchronized {
+
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        MultithreadingSynchronized threadsTesting = new MultithreadingSynchronized();
+        threadsTesting.doWork();
+    }
+
+    void doWork() throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    incrementValue();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    incrementValue();
+                }
+            }
+        });
+        thread.start();
+        thread2.start();
+
+        thread.join();
+        thread2.join();
+
+        System.out.println(counter);
+    }
+
+    private synchronized void incrementValue() {
+        counter += 1;
+    }
+}
