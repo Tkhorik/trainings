9449a6c79b01d55e51dcc5e69100a309f31ac7fd
marv <marvelmd@gmail.com>
2019-03-04 20:15:16 +0300

    ProducerConsumer realization all you need to know tha you should lock your queue on the one object do not lock your threads on .this object

---
+++ src/multithreading/ProducerConsumer.java
@@ -0,0 +1,14 @@
+package multithreading;
+
+public class ProducerConsumer {
+    public static void main(String[] args) {
+        Object o = new Object();
+        ProducerConsumer newwewe = new ProducerConsumer();
+        newwewe.toString();
+
+/*
+         собственая реализайия не требуется в нормальной жизни
+     все что приходится делать с преседаниями уже реализовано в библиотеке канкарент   */
+
+    }
+}
